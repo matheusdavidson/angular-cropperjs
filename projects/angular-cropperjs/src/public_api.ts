c4d6a59af8f1a874ad06782596472ff0da8f52eb
Matheus Davidson <matheusdavidson@gmail.com>
2018-08-09 17:07:58 -0300

    Revert "chore(library): generate lib as a project"
    This reverts commit 4f949d41c3962f2a5ed68897901c56cf403eb27c.

--- projects/angular-cropperjs/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-cropperjs
- */
-
-export * from './lib/angular-cropperjs.service';
-export * from './lib/angular-cropperjs.component';
-export * from './lib/angular-cropperjs.module';
