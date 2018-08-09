4f949d41c3962f2a5ed68897901c56cf403eb27c
Matheus Davidson <matheusdavidson@gmail.com>
2018-08-09 17:01:41 -0300

    chore(library): generate lib as a project

---
+++ projects/angular-cropperjs/src/lib/angular-cropperjs.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'angular-angular-cropperjs',
+  template: `
+    <p>
+      angular-cropperjs works!
+    </p>
+  `,
+  styles: []
+})
+export class AngularCropperjsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/angular-cropperjs/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-cropperjs
+ */
+
+export * from './lib/angular-cropperjs.service';
+export * from './lib/angular-cropperjs.component';
+export * from './lib/angular-cropperjs.module';
