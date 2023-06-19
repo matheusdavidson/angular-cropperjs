ec7da3b4e3717645e001b120c7629a0764bb13d8
Matheus Davidson <matheusdavidson@gmail.com>
2023-06-18 22:09:06 -0300

    feat(demo): improve demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { CropperComponent } from "angular-cropperjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+    @ViewChild(CropperComponent, { static: true })
+    public cropperElement: CropperComponent;
+
+    reset() {
+        this.cropperElement.cropper.reset();
+    }
+
+    goDown() {
+        this.cropperElement.cropper.move(0, 1);
+    }
+
+    goUp() {
+        this.cropperElement.cropper.move(0, -1);
+    }
+
+    goRight() {
+        this.cropperElement.cropper.move(10, 0);
+    }
+
+    goLeft() {
+        this.cropperElement.cropper.move(-10, 0);
+    }
+
+    zoomOut() {
+        this.cropperElement.cropper.zoom(-0.1);
+    }
+
+    zoomIn() {
+        this.cropperElement.cropper.zoom(0.1);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { AngularCropperjsModule } from "angular-cropperjs";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AngularCropperjsModule],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AngularCropperjsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
